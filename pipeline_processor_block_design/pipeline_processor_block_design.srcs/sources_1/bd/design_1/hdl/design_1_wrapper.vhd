b177d5c6b908da1eebb10b53571b198c654f0fb7
uncerso <yayayayaya38@gmail.com>
2020-12-17 00:16:43 +0300

    Added axi_pipe to read reg data

--- pipeline_processor_block_design/pipeline_processor_block_design.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
+++ pipeline_processor_block_design/pipeline_processor_block_design.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.vhd
@@ -1,7 +1,7 @@
 --Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 ----------------------------------------------------------------------------------
 --Tool Version: Vivado v.2019.2.1 (lin64) Build 2729669 Thu Dec  5 04:48:12 MST 2019
---Date        : Wed Dec 16 00:11:07 2020
+--Date        : Wed Dec 16 20:49:45 2020
 --Host        : KillerQueen running 64-bit unknown
 --Command     : generate_target design_1_wrapper.bd
 --Design      : design_1_wrapper
